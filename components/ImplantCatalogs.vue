724fe697d2dd3a770f0d539b5d2c5328cfea50d3
Zachary Burk <zachburk7@gmail.com>
2024-01-08 11:45:03 -0500

    Set up implant catalogs as radio buttons

---
+++ components/ImplantCatalogs.vue
@@ -0,0 +1,34 @@
+<template>
+  <RadioGroup
+    default-value="astra2018"
+    class="flex flex-row"
+    :modelValue="props.selectedCatalogId"
+    @update:model-value="updateValue">
+    <div class="flex items-center space-x-2">
+      <RadioGroupItem id="r1" value="astra2018"/>
+      <Label for="r1">Astra 2018</Label>
+    </div>
+    <div class="flex items-center space-x-2">
+      <RadioGroupItem id="r2" value="astra2023"/>
+      <Label for="r2">Astra 2023</Label>
+    </div>
+    <div class="flex items-center space-x-2">
+      <RadioGroupItem id="r3" value="nobel"/>
+      <Label for="r3">Nobel</Label>
+    </div>
+  </RadioGroup>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    selectedCatalogId: string
+  }>()
+
+  const emit = defineEmits<{
+    (e: 'update:modelValue', value: string): void
+  }>()
+
+  function updateValue(value: string) {
+    emit('update:modelValue', value);
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,20 +2,9 @@
   <div class="min-h-screen">
     <div class="flex flex-col md:flex-row md:space-x-6">
       <div class="order-last mt-10 space-y-2 md:order-1 md:mt-0 md:w-1/2">
-        <RadioGroup default-value="astra2018" v-model="selectedCatalogId" class="flex flex-row">
-          <div class="flex items-center space-x-2">
-            <RadioGroupItem id="r1" value="astra2018"/>
-            <Label for="r1">Astra 2018</Label>
-          </div>
-          <div class="flex items-center space-x-2">
-            <RadioGroupItem id="r2" value="astra2023"/>
-            <Label for="r2">Astra 2023</Label>
-          </div>
-          <div class="flex items-center space-x-2">
-            <RadioGroupItem id="r3" value="nobel"/>
-            <Label for="r3">Nobel</Label>
-          </div>
-        </RadioGroup>
+        <ImplantCatalogs
+          :selectedCatalogId="selectedCatalogId"
+          @update:model-value="(newValue) => selectedCatalogId = newValue" />
 
         <div class="py-1 flex flex-row flex-wrap gap-1">
           <Button
